f72801fc1c4e31c46d5040f8d094fc1d4f726964
Shafiq Islam <smislam@gmail.com>
2016-02-23 14:56:59 -0600

    again

--- image.php
+++ image.php
@@ -5,7 +5,7 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
 
-// Draw a filled rectangle //comment
+// Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 //imagefilledrectangle($im, 0, 0, 199, 199, $green);
 imagefilledrectangle($im, 0, 255, 255, 255, $white);
